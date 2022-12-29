0db3af96fa031ab240fc09a6fa23a7b8946da08c
viettran1010 <viettran10101988@gmail.com>
2022-12-29 13:03:38 +0700

    chore: test migration

--- migrations/1672293058474-update-user-schema-test.js
+++ migrations/1672293058474-update-user-schema-test.js
@@ -1,15 +1,17 @@
-const { MigrationInterface, QueryRunner } = require("typeorm");
+const { MigrationInterface, QueryRunner } = require('typeorm');
 
 module.exports = class updateUserSchema31672293058474 {
-    name = 'updateUserSchema31672293058474'
+  name = 'updateUserSchema31672293058474';
 
-    async up(queryRunner) {
-        await queryRunner.query(`ALTER TABLE "user" ADD "resetPasswordToken" character varying`);
-        await queryRunner.query(`ALTER TABLE "user" ADD "resetPasswordExpires" TIMESTAMP`);
-    }
+  async up(queryRunner) {
+    await queryRunner.query(
+      `ALTER TABLE "user" ADD "resetPasswordToken" TIMESTAMP`,
+    );
+  }
 
-    async down(queryRunner) {
-        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "resetPasswordExpires"`);
-        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "resetPasswordToken"`);
-    }
-}
+  async down(queryRunner) {
+    await queryRunner.query(
+      `ALTER TABLE "user" DROP COLUMN "resetPasswordToken"`,
+    );
+  }
+};
