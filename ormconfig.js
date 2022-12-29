fdae740b491cb276b414f11b391430f3206fdcf4
viettran1010 <viettran10101988@gmail.com>
2022-12-29 08:32:05 +0700

    chore: config

--- ormconfig.js
+++ ormconfig.js
@@ -25,6 +25,15 @@ switch (process.env.NODE_ENV) {
 
     break;
   case 'production':
+    Object.assign(dbConfig, {
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      migrationsRun: true,
+      entities: ['**/*.entity.js'],
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    });
   default:
     throw new Error('unknown environment');
 }
