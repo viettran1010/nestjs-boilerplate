aea5b8a361a4cdb261348b3cd12ad4099d71529b
viettran1010 <viettran10101988@gmail.com>
2022-12-29 11:56:22 +0700

    chore: test migration

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -26,6 +26,9 @@ export class User {
   @OneToMany(() => Report, (report) => report.user) // 1st arg to solve circular dependency
   reports: Report[];
 
+  @Column({ nullable: true })
+  resetPasswordToken?: string;
+
   @AfterInsert()
   logInsert() {
     console.log('Inserted user with id: ', this.id);
