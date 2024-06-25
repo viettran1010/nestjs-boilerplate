b3e17f9a68209a24a008ab87e24a74e14d155322
Lettie (Jitera) <developers@jitera.com>
2024-06-25 05:53:29 +0000

    feat[api]: implement Create turtle entry

--- src/turtles/turtle.entity.ts
+++ src/turtles/turtle.entity.ts
@@ -1,5 +1,12 @@
 import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import { IsString, IsNumber, IsEnum } from 'class-validator';
 import { Location } from '../locations/location.entity';
+
+export enum HealthStatus {
+  HEALTHY = 'healthy',
+  SICK = 'sick',
+  RECOVERING = 'recovering',
+}
 
 @Entity()
 export class Turtle {
@@ -12,16 +19,20 @@ export class Turtle {
   @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
   updated_at: Date;
 
+  @IsString()
   @Column({ type: 'varchar' })
   species: string;
 
+  @IsNumber()
   @Column({ type: 'int' })
   age: number;
 
+  @IsEnum(HealthStatus)
   @Column({ type: 'varchar' })
-  health_status: string;
+  health_status: HealthStatus;
 
   @ManyToOne(() => Location, (location) => location.turtles)
+  @IsNumber()
   @Column({ type: 'int' })
   location_id: number;
 }
