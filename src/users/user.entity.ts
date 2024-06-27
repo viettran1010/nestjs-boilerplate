79444416b110164afd8a1a48a78e5330bd93af7d
Danita (Jitera) <developers@jitera.com>
2024-06-27 03:32:26 +0000

    feat[database]: update table 'users' and create table 'menu_options' and create table 'user_permissions' Changes * '/src/menu-options/menu-option.entity.ts': created * '/migrations/1719459032961-create-menu-options-table.js': created * '/src/user-permissions/user-permission.entity.ts': created * '/migrations/1719459032961-create-user-permissions-table.js': created * '/src/users/user.entity.ts': updated * '/migrations/1719459032961-update-users-table.js': updated

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,5 +1,8 @@
 import { Report } from '../reports/report.entity';
 import { Student } from '../students/student.entity';
+import { Contract } from '../contracts/contract.entity';
+import { ContractAction } from '../contract_actions/contract_action.entity';
+import { AuditLog } from '../audit_logs/audit_log.entity';
 import {
   Entity,
   Column,
@@ -8,6 +11,7 @@ import {
   AfterUpdate,
   AfterRemove,
   OneToMany,
+  ManyToOne,
 } from 'typeorm';
 
 @Entity()
@@ -24,7 +28,19 @@ export class User {
   @Column()
   password: string;
 
-  @OneToMany(() => Report, (report) => report.user) // 1st arg to solve circular dependency
+  @ManyToOne(() => Contract, contract => contract.user, { nullable: true })
+  @Column({ nullable: true })
+  contract_id?: number;
+
+  @ManyToOne(() => ContractAction, contractAction => contractAction.user, { nullable: true })
+  @Column({ nullable: true })
+  contract_action_id?: number;
+
+  @ManyToOne(() => AuditLog, auditLog => auditLog.user, { nullable: true })
+  @Column({ nullable: true })
+  audit_log_id?: number;
+
+  @OneToMany(() => Report, (report) => report.user)
   @OneToMany(() => Student, (student) => student.user)
   students: Student[];
 
