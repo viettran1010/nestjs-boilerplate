23a32d2104475fa7e08038662190b3710c879474
Catharine (Jitera) <developers@jitera.com>
2024-06-27 06:48:39 +0000

    feat[api]: implement Validate Customer Registration

---
+++ src/customers/dto/create-customer.dto.ts
@@ -0,0 +1,36 @@
+import { IsString, IsNotEmpty, IsEmail, IsDate, ValidateIf, isDateString, isFuture } from 'class-validator';
+
+export class CreateCustomerDto {
+  @IsNotEmpty({ message: 'Name is required.' })
+  @IsString()
+  name: string;
+
+  @IsNotEmpty({ message: 'Name in Katakana is required.' })
+  @IsString()
+  name_katakana: string;
+
+  @IsNotEmpty({ message: 'Company name is required.' })
+  @IsString()
+  company_name: string;
+
+  @IsNotEmpty({ message: 'Zip code is required.' })
+  @IsString()
+  zip_code: string;
+
+  @IsNotEmpty({ message: 'Address is required.' })
+  @IsString()
+  address: string;
+
+  @IsNotEmpty({ message: 'Phone number is required.' })
+  @IsString()
+  phone_number: string;
+
+  @IsNotEmpty({ message: 'Email address is required and must be valid.' })
+  @IsEmail()
+  email_address: string;
+
+  @IsNotEmpty({ message: 'Date of birth is required and must be a valid date not in the future.' })
+  @IsDate()
+  @ValidateIf((o) => !isDateString(o.date_of_birth) || isFuture(o.date_of_birth))
+  date_of_birth: Date;
+}
