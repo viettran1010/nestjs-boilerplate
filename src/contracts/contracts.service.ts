20e0c2ba8eeebeb60a86dfdbe000f593b74e0675
Jacqueline (Jitera) <developers@jitera.com>
2024-06-27 03:39:57 +0000

    feat[api]: implement Update contract status to approved

---
+++ src/contracts/contracts.service.ts
@@ -0,0 +1,27 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Contract } from './contracts.entity';
+import { ContractActionsService } from '../contract_actions/contract_actions.service';
+
+@Injectable()
+export class ContractsService {
+  constructor(
+    @InjectRepository(Contract)
+    private contractRepository: Repository<Contract>,
+    private contractActionsService: ContractActionsService
+  ) {}
+
+  // ... other service methods
+
+  async approveContract(contractId: number, userId: number): Promise<Contract> {
+    const contract = await this.contractRepository.findOneBy({ id: contractId });
+    if (!contract) {
+      throw new NotFoundException(`Contract with ID ${contractId} not found`);
+    }
+    contract.status = 'Approved';
+    await this.contractRepository.save(contract);
+    await this.contractActionsService.recordAction(contractId, userId, 'Approved');
+    return contract;
+  }
+}
