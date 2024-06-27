87f12c4e5c9147e43914e7c0821da3f2009ba390
Norean (Jitera) <developers@jitera.com>
2024-06-27 06:28:09 +0000

    feat[database]: update table 'contracts' and update table 'account_type_informations' Changes * '/src/contracts/contract.entity.ts': updated * '/migrations/1719469544381-update-contracts-table.js': updated * '/src/account-type-informations/account-type-information.entity.ts': updated * '/migrations/1719469544381-update-account-type-informations-table.js': updated

--- src/account-type-informations/account-type-information.entity.ts
+++ src/account-type-informations/account-type-information.entity.ts
@@ -1,9 +1,22 @@
-import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, ManyToOne, JoinColumn } from 'typeorm';
+import { User } from '../users/user.entity';
+import { Contract } from '../contracts/contract.entity';
 
 @Entity()
 export class AccountTypeInformation {
   @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({ type: 'varchar', nullable: true })
+  currencyDeposited: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'user_id' })
+  userId: User;
+
+  @ManyToOne(() => Contract)
+  @JoinColumn({ name: 'contract_id' })
+  contractId: Contract;
 
   @CreateDateColumn()
   created_at: Date;
