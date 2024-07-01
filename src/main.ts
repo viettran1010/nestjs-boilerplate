858a545cd79d0827b0c934ba5f6fb29d5d430590
Eudora (Jitera) <developers@jitera.com>
2024-07-01 03:08:06 +0000

    fix: 5 issues from deployment report

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { AllExceptionsFilter } from './filters/all-exceptions.filter';
+import { AllExceptionsFilter } from './filters/all-exceptions-filter';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
