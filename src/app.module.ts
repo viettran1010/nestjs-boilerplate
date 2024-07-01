fc9dc713d1ee6f14fc5503fc1e5da76fd6498464
Annabell (Jitera) <developers@jitera.com>
2024-07-01 03:06:10 +0000

    fix: 3 issues from deployment report

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,8 +6,10 @@ import { AppService } from './app.service';
 import { ReportsModule } from './reports/reports.module';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { User } from './users/user.entity';
+import { Report } from './reports/report.entity';
 import { APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { CurrentUserInterceptor } from './users/interceptors/current-user.interceptor';
 import { JanitorModule } from './janitor/janitor.module';
 const cookieSession = require('cookie-session');
@@ -28,26 +30,29 @@ const cookieSession = require('cookie-session');
     UsersModule,
     ReportsModule,
     TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        // ... other options
-        type: 'postgres', // or another database type
-        // ... other options
-      }),
+      useFactory: () => {
+        // Example TypeOrmModuleOptions, should be replaced with actual configuration
+        return {
+          type: 'postgres', // or another database type
+          // other options
+        };
+      },
     }),
     JanitorModule,
-    // Other imports...
-  ],
-  controllers: [AppController],
-  providers: [
     AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CurrentUserInterceptor,
+    },
     {
       provide: APP_PIPE,
       useClass: ValidationPipe,
     },
     {
-      provide: APP_INTERCEPTOR,
-      useClass: CurrentUserInterceptor,
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+      }),
     },
   ],
 })
