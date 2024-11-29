15daa3eaf6e9503d486b7a5bc01e0f52630b690c
viettran1010 <viettran10101988@gmail.com>
2024-11-29 12:42:10 +0700

    change

--- src/janitor/janitor.controller.ts
+++ src/janitor/janitor.controller.ts
@@ -1,4 +1,4 @@
-awdawimport { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+awwaawdawimport { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { JanitorService } from './janitor.service';
 import { CreateJanitorDto } from './dto/create-janitor.dto';
 import { UpdateJanitorDto } from './dto/update-janitor.dto';
